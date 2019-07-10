de8a27a6531dd6c94755f48f9d8fc209d10d7f26
Ryan <8942922+rajose@users.noreply.github.com>
2019-07-10 09:04:07 -0400

    Added First component to work on
    - Used ng generate to make Birthday, to keep track of friends bdays
    - Also made investments for the next module to work on for keeping track of
      net worth
    - Added routing to link to bday component, will later have bday as a
      library (?) and instead link to a grid-like component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { BirthdayComponent } from './birthday/birthday.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BirthdayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: BirthdayComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
